2a7bedb56e47a1d273f5c8df8d8fdb63b6df2475
Martin Ruckli <martin@ruck.li>
2015-06-10 16:29:16 +0200

    adding eureka server

---
+++ eureka-server/src/main/java/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
