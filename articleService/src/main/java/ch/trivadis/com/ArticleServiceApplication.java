8be927c8304b5f579d82587981d37193e571f305
Martin Ruckli <martin@ruck.li>
2015-06-10 16:32:13 +0200

    configuring via config-server Implementing Simple check if the comment service is available, if so return the uri as comment link

--- articleService/src/main/java/ch/trivadis/com/ArticleServiceApplication.java
+++ articleService/src/main/java/ch/trivadis/com/ArticleServiceApplication.java
@@ -2,8 +2,14 @@ package ch.trivadis.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableZuulProxy
 public class ArticleServiceApplication {
 
     public static void main(String[] args) {
--- articleService/src/main/java/ch/trivadis/com/service/ArticleService.java
+++ articleService/src/main/java/ch/trivadis/com/service/ArticleService.java
@@ -1,34 +1,51 @@
 package ch.trivadis.com.service;
 
-import ch.trivadis.com.entities.Article;
-import ch.trivadis.com.repository.ArticleRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
+import ch.trivadis.com.entities.Article;
+import ch.trivadis.com.repository.ArticleRepository;
 
 /**
  * Created by Andy Moncsek on 10.06.15.
  */
 @RestController
 public class ArticleService {
-    @Inject
-    private ArticleRepository repository;
+	@Inject
+	private ArticleRepository repository;
 
-    @RequestMapping("/articleService/findAll")
-    public List<Article> findAll() {
-        return new ArrayList<>(repository.getAllArticles());
-    }
-    @RequestMapping("/articleService/findMAX/{amount}")
-    public List<Article>  findMax(@PathVariable(value="amount") String amount) {
-        return new ArrayList<>(repository.getAllArticles(Integer.valueOf(amount)));
-    }
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
-    @RequestMapping("/articleService/findPage/{page}")
+	@RequestMapping("/articleService/findAll")
+	public List<Article> findAll() {
+		return new ArrayList<>(repository.getAllArticles());
+	}
+
+	@RequestMapping("/articleService/findMAX/{amount}")
+	public List<Article> findMax(@PathVariable(value = "amount") String amount) {
+		return new ArrayList<>(repository.getAllArticles(Integer
+				.valueOf(amount)));
+	}
+
+	@RequestMapping("/articleService/findPage/{page}")
     public List<Article>  findPage(@PathVariable(value="page") String page) {
-        return new ArrayList<>(repository.getAllArticlesPages(Integer.valueOf(page)));
+		ArrayList<Article> articles = new ArrayList<>(repository.getAllArticlesPages(Integer.valueOf(page)));
+		Optional<ServiceInstance> firstService = discoveryClient.getInstances("comment-service").stream().findFirst();
+		if(firstService.isPresent()) {
+        	return articles.stream().map(a -> new Article(a, firstService.get().getUri().toString())).collect(Collectors.toList());
+        }
+        return articles;
     }
 }
